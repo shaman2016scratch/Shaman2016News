fe8494afddd5e60e2461bbbca4735c4bd9e61ecf
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-10-31 00:12:32 +0300

    Update news.js

--- js/news.js
+++ js/news.js
@@ -13,6 +13,7 @@ newsList = fetch("https://shaman2016scratch.github.io/Shaman2016News/api/NewsLis
 news = "<h2>Статьи</h2>"
 for(i = 0; i < newsList.length; i++) {
   ii = newsList[i]
-  news = `${news}<div class="project"><h3>${ii.name}</h3><p>${ii.description}</p><p>by ${ii.creator}</p><a href="/Shaman2016News/news/${ii.id}">Открыть статью</a><a href="${ii.post}">Открыть пост</a>`
+  news = `${news}<div class="project"><h3>${ii.name}</h3><p>${ii.description}</p><p>by ${ii.creator}</p><a href="/Shaman2016News/news/${ii.id}">Открыть статью</a><a href="${ii.post}">Открыть пост</a></div>`
+  console.log(i)
 }
 innerHTML('news', news)
