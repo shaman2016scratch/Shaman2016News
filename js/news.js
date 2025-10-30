462057650d1bf7a4f9bc74f3844d19bd10d7003e
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-10-31 00:29:28 +0300

    Update news.js

--- js/news.js
+++ js/news.js
@@ -4,16 +4,10 @@ function innerHTML(p1, p2) {
 function TextContent(p1, p2) {
   document.getElementById(p1).textContent = p2
 }
-newsList = fetch("https://shaman2016scratch.github.io/Shaman2016News/api/NewsList.js", {  
+news = fetch("https://shaman2016scratch.github.io/Shaman2016News/api/NewsList2.js", {  
   method: 'GET',  
   headers: { 
     "Content-Type": "application/json",  
   }
 })
-news = "<h2>Статьи</h2>"
-for(i = 0; i < newsList.length; i++) {
-  ii = newsList[i]
-  news = `${news}<div class="project"><h3>${ii.name}</h3><p>${ii.description}</p><p>by ${ii.creator}</p><a href="/Shaman2016News/news/${ii.id}">Открыть статью</a><a href="${ii.post}">Открыть пост</a></div>`
-  console.log(i)
-}
 innerHTML('news', news)
