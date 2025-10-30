7131a57d472466a0018e5bf7a70c7aafe49ce707
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-10-30 23:42:45 +0300

    Create NewsList.js

---
+++ api/NewsList.js
@@ -0,0 +1,18 @@
+[
+  {
+    "id": "1",
+    "pubdata": ["30","10","2025","22","50"],
+    "creator": "SHAMAN2016",
+    "name": "Турбоварп заблокировали в России",
+    "description": "TurboWarp был заблокирован на теретории Российской Федерации",
+    "post": "https://t.me/shaman2016scratcher/388",
+  },
+  {
+    "id": "2",
+    "pubdata": ["30","10","2025","22","55"],
+    "creator": "SHAMAN2016",
+    "name": "SMS от телеграма и WhatsApp были заблокированы в России",
+    "description": "Российские операторы стали блокировать SMS от Telegram и WhatsApp. Подробности.",
+    "post": "https://t.me/shaman2016scratcher/389",
+  },
+]
