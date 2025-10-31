d4aeab8a40194d75cc271cd1bc399783beceea72
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-10-31 13:56:58 +0300

    Update info.js

--- js/info.js
+++ js/info.js
@@ -4,5 +4,9 @@ function innerHTML(p1, p2) {
 function TextContent(p1, p2) {
   document.getElementById(p1).textContent = p2
 }
-toppanel = ``
+toppanel = `
+  <a href="/shaman2016.github.io/">Сайт SHAMAN2016</a>
+  <a href="/Shaman2016News">Shaman2016News</a>
+  <a href="https://t.me/s/shaman2016scratcher?q=%23Shaman2016News">Посты</a>
+`
 innerHTML('top-panel', toppanel)
