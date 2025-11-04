0f6e8d7d1b61b27db0e745e33930545ebaae9949
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-04 13:28:10 +0300

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,8 +1,29 @@
+docData = {}
+docData.theme = document.getElementById('theme')
+docData.css = document.getElementById('css')
 theme = localStorage.getItem('theme')
 if (!theme) {
   localStorage.setItem('theme', 'light')
 }
 theme = localStorage.getItem('theme')
+themeCSS = {
+  "light": "main.css",
+  "dark": "main2.css",
+}
+themeIcon = {
+  "light": "/DeytelengScript/tema-1.svg",
+  "dark": "/DeytelengScript/tema-2.svg",
+}
+function UpdateTheme(th) {
+  docData.theme.innerHTML = themeIcon[th]
+  docData.css.innerHTML = themeCSS[th]
+}
+UpdateTheme(theme)
 function updateTheme() {
-  
+  if (theme === 'dark') {
+    theme = 'light'
+  } else {
+    theme = 'dark'
+  }
+  UpdateTheme(theme)
 }
