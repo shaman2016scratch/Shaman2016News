ea3416272b87885aeb74528ae260ac31a078add4
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-04 13:14:56 +0300

    Update news.js

--- js/news.js
+++ js/news.js
@@ -38,5 +38,21 @@ news = `
     <a href="https://t.me/shaman2016scratcher/405"><button>Смотреть пост</button></a>
     <p>Опубликовано 31.10.2025 в 22:16:04 UTC+3</p>
   </div>
+  <div class="project">
+    <h3>RussiaScript — новый язык программирования!</h3>
+    <p>RussiaScript — Новый русский язык программирования. Подробности в статье</p>
+    <p>От SHAMAN2016</p>
+    <a href="/Shaman2016News/news/5"><button>Смотреть статью</button></a>
+    <a href="https://t.me/shaman2016scratcher/419"><button>Смотреть пост</button></a>
+    <p>Опубликовано 02.11.2025 в 21:21:10 UTC+3</p>
+  </div>
+  <div class="project">
+    <h3>RussiaScript 1.1 оффициально вышел</h3>
+    <p>RussiaScript 1.1 вышел оффициально, подробности</p>
+    <p>От SHAMAN2016</p>
+    <a href="/Shaman2016News/news/6"><button>Смотреть статью</button></a>
+    <a href="https://t.me/shaman2016scratcher/422"><button>Смотреть пост</button></a>
+    <p>Опубликовано 03.11.2025 в 14:24:16 UTC+3</p>
+  </div>
 `
 innerHTML('news', news)
