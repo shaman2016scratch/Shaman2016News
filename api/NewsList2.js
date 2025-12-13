2265db7e40ef361260f127f53e8a69f4d6bab947
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-12-13 18:27:08 +0300

    Update NewsList2.js

--- api/NewsList2.js
+++ api/NewsList2.js
@@ -3,13 +3,13 @@
   <h3>Турбоварп заблокировали в России</h3>
   <p>TurboWarp был заблокирован на теретории Российской Федерации</p>
   <p>От SHAMAN2016</p>
-  <a href="/news/1"><button>Смотреть статью</button></a>
+  <a href="/Shaman2016News/news/1"><button>Смотреть статью</button></a>
   <a href="https://t.me/shaman2016scratcher/388"><button>Смотреть пост</button></a>
 </div>
 <div class="project">
   <h3>SMS от телеграма и WhatsApp были заблокированы в России</h3>
   <p>Российские операторы стали блокировать SMS от Telegram и WhatsApp. Подробности.</p>
   <p>От SHAMAN2016</p>
-  <a href="/news/2"><button>Смотреть статью</button></a>
+  <a href="/Shaman2016News/news/2"><button>Смотреть статью</button></a>
   <a href="https://t.me/shaman2016scratcher/389"><button>Смотреть пост</button></a>
 </div>
