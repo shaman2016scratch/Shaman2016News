e7ba62a492f2a8ee0047338fec67fa88a3f03fc0
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-12-30 21:07:25 +0300

    Update NewYear.js

--- NewYear.js
+++ NewYear.js
@@ -4,12 +4,9 @@ const url = new URL(window.location)
 const searchParams = url.searchParams
 let NewYearFunc = {}
 if (searchParams.get("NewYearMode") || NewYear) {
-  txt = "<p>Happy New Year!</p><p>We say goodbye to the outgoing year 2025 and look forward to the coming year 2026!</p><p>Прощаемся с уходящим 2025 годом и приветствуем и радуемся наступающему 2026 году</p>"
-  document.getElementById('NewYear').innerHTML = txt
+  document.getElementById('NewYear').innerHTML = "<p>Happy New Year!</p><p>We say goodbye to the outgoing year 2025 and look forward to the coming year 2026!</p><p>Прощаемся с уходящим 2025 годом, а также приветствуем и радуемся наступающему 2026 году</p>"
 } else if (NewYearMode === 0) {
-  txt = "<p>Скоро новый год! The new year is coming soon!</p>"
-  document.getElementById('NewYear').innerHTML = txt
+  document.getElementById('NewYear').innerHTML = "<p>Скоро новый год! The new year is coming soon!</p>"
 } else if (NewYearMode === 1) {
-  txt = "<p>С наступающим годом! Happy new year 2026!</p>"
-  document.getElementById('NewYear').innerHTML = txt
+  document.getElementById('NewYear').innerHTML = "<p>С наступающим годом! Happy new year 2026!</p>"
 }
