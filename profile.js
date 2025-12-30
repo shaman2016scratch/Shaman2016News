e7ba87d5d1ae60dfc6f08d121ba0c4aa61b150ad
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-12-30 21:33:50 +0300

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,7 +1,7 @@
 const url = new URL(window.location)
 const searchParams = url.searchParams
-user = searchParams.get('user')
-users = [
+let user = searchParams.get('user')
+const users = [
   "SHAMAN2016",
   "UpdateBot",
 ]
@@ -12,6 +12,8 @@ if (user == 'SHAMAN2016') {
   document.getElementById('desc').textContent = 'Профиль SHAMAN2016 в Shaman2016News'
   document.getElementById('title2').textContent = 'SHAMAN2016 • Shaman2016News profiles'
   document.getElementById('info').innerHTML = '<p>Привет! Я SHAMAN2016, скретчер из России! Также я web разработчик и JavaScript разработчик!</p><p>Также я создатель Shaman2016News</p><p>Мой сайт: shaman2016scratch.github.io (да, Shaman2016News размещен на нём)</p>'
+  document.getElementById('Vklad').innerHTML = '<p>Создал 10 статей</p><p>Создал категории, профиля и саму площадку</p><p>Делал новогодний режим в 2025 году</p>'
+  document.getElementById('Dostizhenia').innerHTML = '<p>Создатель</p><p>Программист</p><p>Новостной (10/5 статей)</p>'
 } else if (user == 'UpdateBot') {
   document.getElementById('title').textContent = 'UpdateBot • Shaman2016News Profiles'
   document.getElementById('ogtitle').textContent = 'UpdateBot'
