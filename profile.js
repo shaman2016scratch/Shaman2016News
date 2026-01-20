c16fefee48e49ac8f16f6dd4967d1fc4c8c07de8
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-01-20 22:48:56 +0300

    Update profile.js

--- profile.js
+++ profile.js
@@ -8,24 +8,24 @@ const users = [
 if (user == 'SHAMAN2016') {
   document.getElementById('title').textContent = 'SHAMAN2016 • Shaman2016News Profiles'
   document.getElementById('ogtitle').content = 'SHAMAN2016'
-  document.getElementById('ogdesc').textContent = 'SHAMAN2016 profile in Shaman2016News'
-  document.getElementById('desc').textContent = 'Профиль SHAMAN2016 в Shaman2016News'
+  document.getElementById('ogdesc').content = 'SHAMAN2016 profile in Shaman2016News'
+  document.getElementById('desc').content = 'Профиль SHAMAN2016 в Shaman2016News'
   document.getElementById('title2').textContent = 'SHAMAN2016 • Shaman2016News profiles'
   document.getElementById('info').innerHTML = '<p>Привет! Я SHAMAN2016, скретчер из России! Также я web разработчик и JavaScript разработчик!</p><p>Также я создатель Shaman2016News</p><p>Мой сайт: shaman2016scratch.github.io (да, Shaman2016News размещен на нём)</p>'
   document.getElementById('Vklad').innerHTML = '<p>Создал 10 статей</p><p>Создал категории, профиля и саму площадку</p><p>Делал новогодний режим в 2025 году</p>'
   document.getElementById('Dostizhenia').innerHTML = '<p>Создатель</p><p>Программист</p><p>Новостной (10/5 статей)</p>'
 } else if (user == 'UpdateBot') {
   document.getElementById('title').textContent = 'UpdateBot • Shaman2016News Profiles'
-  document.getElementById('ogtitle').textContent = 'UpdateBot'
-  document.getElementById('ogdesc').textContent = 'UpdateBot profile in Shaman2016News'
-  document.getElementById('desc').textContent = 'Профиль UpdateBot в Shaman2016News'
+  document.getElementById('ogtitle').content = 'UpdateBot'
+  document.getElementById('ogdesc').content = 'UpdateBot profile in Shaman2016News'
+  document.getElementById('desc').content = 'Профиль UpdateBot в Shaman2016News'
   document.getElementById('title2').textContent = 'UpdateBot • Shaman2016News profiles'
   document.getElementById('info').innerHTML = '<p>Добро пожаловать в профиль UpdateBot [bot]</p><p>UpdateBot выкладывает статьи о обновлениях на shaman2016scratch.github.io/Shaman2016News/*.</p>'
 } else {
   document.getElementById('title').textContent = `${user} • Shaman2016News Profiles`
-  document.getElementById('ogtitle').textContent = user
-  document.getElementById('ogdesc').textContent = `${user} profile in Shaman2016News`
-  document.getElementById('desc').textContent = `Профиль ${user} в Shaman2016News`
+  document.getElementById('ogtitle').content = user
+  document.getElementById('ogdesc').content = `${user} profile in Shaman2016News`
+  document.getElementById('desc').content = `Профиль ${user} в Shaman2016News`
   document.getElementById('title2').textContent = `${user} • Shaman2016News profiles`
   document.getElementById('info').innerHTML = '<p>Данный профиль не найден. 404</p>'
 }
