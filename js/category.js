06ae238e7ff37efc9259c3495fa4114d362c8331
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-01-21 23:52:59 +0300

    Update category.js

--- js/category.js
+++ js/category.js
@@ -2,31 +2,26 @@ const url = new URL(window.location)
 const searchParams = url.searchParams
 let categ = searchParams.get('name')
 const categories = [
-  "Categories",
-  "News",
-  "IT",
-  "Scratch",
-  "Telegram",
+  "categories",
+  "news",
+  "it",
+  "scratch",
+  "telegram",
   "users",
 ]
-if (false) {
-  document.getElementById('title').textContent = 'SHAMAN2016 • Shaman2016News Profiles'
-  document.getElementById('ogtitle').content = 'SHAMAN2016'
-  document.getElementById('ogdesc').content = 'SHAMAN2016 profile in Shaman2016News'
-  document.getElementById('desc').content = 'Профиль SHAMAN2016 в Shaman2016News'
-  document.getElementById('title2').textContent = 'SHAMAN2016 • Shaman2016News profiles'
-  document.getElementById('info').innerHTML = '<p>Привет! Я SHAMAN2016, скретчер из России! Также я web разработчик и JavaScript разработчик!</p><p>Также я создатель Shaman2016News</p><p>Мой сайт: shaman2016scratch.github.io (да, Shaman2016News размещен на нём)</p>'
-  document.getElementById('Vklad').innerHTML = '<p>Создал 10 статей</p><p>Создал категории, профиля и саму площадку</p><p>Делал новогодний режим в 2025 году</p>'
-  document.getElementById('Dostizhenia').innerHTML = '<p>Создатель</p><p>Программист</p><p>Новостной (10/5 статей)</p>'
+if (categ === categories [0]) {
+  document.getElementById('title').textContent = 'Categories • Shaman2016News Categories'
+  document.getElementById('ogtitle').content = 'Categories'
+  document.getElementById('ogdesc').content = '"Categories" category in Shaman2016News'
+  document.getElementById('desc').content = 'Категория "Категории" в Shaman2016News'
+  document.getElementById('title2').textContent = 'Categories • Shaman2016News Categories'
+  document.getElementById('info').innerHTML = '<p>Это категория со списком категорий в Shaman2016News</p>'
+  document.getElementById('stati').innerHTML = `<p>Нум... Тут не статьи а категории, но держи: ${categories}</p>`
 } else {
-  document.getElementById('title').textContent = `${user} • Shaman2016News Profiles`
-  document.getElementById('ogtitle').content = user
-  document.getElementById('ogdesc').content = `${user} profile in Shaman2016News`
-  document.getElementById('desc').content = `Профиль ${user} в Shaman2016News`
-  document.getElementById('title2').textContent = `${user} • Shaman2016News profiles`
-  if (user === "all") {
-    document.getElementById('info').innerHTML = users
-  } else {
-    document.getElementById('info').innerHTML = '<p>Данный профиль не найден. 404</p>'
-  }
+  document.getElementById('title').textContent = `${categ} • Shaman2016News Categories`
+  document.getElementById('ogtitle').content = categ
+  document.getElementById('ogdesc').content = `${categ} category in Shaman2016News`
+  document.getElementById('desc').content = `Категория "${categ}" в Shaman2016News`
+  document.getElementById('title2').textContent = `${categ} • Shaman2016News Categories`
+  document.getElementById('info').innerHTML = '<p>Данная категория не найдена. 404</p>'
 }
