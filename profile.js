ca09a7e9a18fcacf06a005e2df6ad2ef8f3272f7
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-01-21 11:07:55 +0300

    Update profile.js

--- profile.js
+++ profile.js
@@ -5,7 +5,7 @@ const users = [
   "SHAMAN2016",
   "UpdateBot",
 ]
-if (user == 'SHAMAN2016') {
+if (user === 'SHAMAN2016') {
   document.getElementById('title').textContent = 'SHAMAN2016 • Shaman2016News Profiles'
   document.getElementById('ogtitle').content = 'SHAMAN2016'
   document.getElementById('ogdesc').content = 'SHAMAN2016 profile in Shaman2016News'
@@ -14,7 +14,7 @@ if (user == 'SHAMAN2016') {
   document.getElementById('info').innerHTML = '<p>Привет! Я SHAMAN2016, скретчер из России! Также я web разработчик и JavaScript разработчик!</p><p>Также я создатель Shaman2016News</p><p>Мой сайт: shaman2016scratch.github.io (да, Shaman2016News размещен на нём)</p>'
   document.getElementById('Vklad').innerHTML = '<p>Создал 10 статей</p><p>Создал категории, профиля и саму площадку</p><p>Делал новогодний режим в 2025 году</p>'
   document.getElementById('Dostizhenia').innerHTML = '<p>Создатель</p><p>Программист</p><p>Новостной (10/5 статей)</p>'
-} else if (user == 'UpdateBot') {
+} else if (user === 'UpdateBot') {
   document.getElementById('title').textContent = 'UpdateBot • Shaman2016News Profiles'
   document.getElementById('ogtitle').content = 'UpdateBot'
   document.getElementById('ogdesc').content = 'UpdateBot profile in Shaman2016News'
@@ -27,5 +27,9 @@ if (user == 'SHAMAN2016') {
   document.getElementById('ogdesc').content = `${user} profile in Shaman2016News`
   document.getElementById('desc').content = `Профиль ${user} в Shaman2016News`
   document.getElementById('title2').textContent = `${user} • Shaman2016News profiles`
-  document.getElementById('info').innerHTML = '<p>Данный профиль не найден. 404</p>'
+  if (user === "all") {
+    document.getElementById('info').innerHTML = users
+  } else {
+    document.getElementById('info').innerHTML = '<p>Данный профиль не найден. 404</p>'
+  }
 }
