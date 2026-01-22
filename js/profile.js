574ef257f1f1da3f498ac12aa1f87df08d98cb3b
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-01-22 13:30:15 +0300

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -7,10 +7,12 @@ const users = [
 ]
 const user_info = {
   "SHAMAN2016": {
-    "colvostat": 11
+    "colvostat": 11,
+    "colvocateg": 5
   },
   "UpdateBot": {
-    "colvostat": 1
+    "colvostat": 1,
+    "colvocateg": 0
   }
 }
 if (user === 'SHAMAN2016' || user === 'polzovatel_8787' || user === 8787 || user === "8787" || user === "shaman2016") {
@@ -20,8 +22,8 @@ if (user === 'SHAMAN2016' || user === 'polzovatel_8787' || user === 8787 || user
   document.getElementById('desc').content = 'Профиль SHAMAN2016 в Shaman2016News'
   document.getElementById('title2').textContent = 'SHAMAN2016 • Shaman2016News profiles'
   document.getElementById('info').innerHTML = `<p>Привет! Я SHAMAN2016, скретчер из России! Также я web разработчик и JavaScript разработчик!</p><p>Также я создатель Shaman2016News</p><p>Мой сайт: shaman2016scratch.github.io (да, Shaman2016News размещен на нём)</p>`
-  document.getElementById('Vklad').innerHTML = `<p>Создал ${user_info.SHAMAN2016.colvostat} статей</p><p>Создал категории, профиля и саму площадку</p><p>Делал новогодний режим в 2025 году</p>`
-  document.getElementById('Dostizhenia').innerHTML = `<p>Создатель</p><p>Программист</p><p>Новостной (${user_info.SHAMAN2016.colvostat}/5 статей)</p>`
+  document.getElementById('Vklad').innerHTML = `<p>Создал ${user_info.SHAMAN2016.colvostat} статей</p><p>Создал ${user_info.SHAMAN2016.colvocateg} категорий</p><p>Создал категории, профиля и саму площадку</p><p>Делал новогодний режим в 2025 году</p>`
+  document.getElementById('Dostizhenia').innerHTML = `<p>Создатель</p><p>Программист</p><p>Новостной (${user_info.SHAMAN2016.colvostat}/5 статей)</p><p>Категорист (${user_info.SHAMAN2016.colvocateg}/5 категорий)</p>`
 } else if (user === 'UpdateBot') {
   document.getElementById('title').textContent = 'UpdateBot • Shaman2016News Profiles'
   document.getElementById('ogtitle').content = 'UpdateBot'
@@ -29,7 +31,7 @@ if (user === 'SHAMAN2016' || user === 'polzovatel_8787' || user === 8787 || user
   document.getElementById('desc').content = 'Профиль UpdateBot в Shaman2016News'
   document.getElementById('title2').textContent = 'UpdateBot • Shaman2016News profiles'
   document.getElementById('info').innerHTML = '<p>Добро пожаловать в профиль UpdateBot [bot]</p><p>UpdateBot выкладывает статьи о обновлениях на shaman2016scratch.github.io/Shaman2016News/*.</p>'
-  document.getElementById('Vklad').innerHTML = `<p>Создал ${user_infoUpdateBot.colvostat} статей</p>`
+  document.getElementById('Vklad').innerHTML = `<p>Создал ${user_info.UpdateBot.colvostat} статей</p>`
 } else {
   document.getElementById('title').textContent = `${user} • Shaman2016News Profiles`
   document.getElementById('ogtitle').content = user
