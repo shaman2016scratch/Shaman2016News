d736ef23f3fc7c984708aeec15f33a0e02dc6896
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-02-14 18:08:52 +0300

    Update preview.js

--- js/preview.js
+++ js/preview.js
@@ -1,2 +1,10 @@
 let title = document.getElementById("titlePage")
 let page = document.getElementById("page")
+const url = new URL(window.location)
+const sP = url.searchParams
+let pagee = sp("page")
+if (false) {
+  console.error("JS is broken, it executes if (false).")
+} else {
+pafe.innerHTML = ``
+}
